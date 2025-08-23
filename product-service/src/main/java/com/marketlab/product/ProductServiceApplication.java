f84fbf6556f0f38f8f18fe00fe1cb7ae2ac75489
Vaneth <vanemorenatropicana@gmail.com>
2025-08-23 17:41:14 -0300

    Create ProductServiceApplication.java

---
+++ product-service/src/main/java/com/marketlab/product/ProductServiceApplication.java
@@ -0,0 +1,11 @@
+package com.marketlab.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class ProductServiceApplication {
+  public static void main(String[] args) { SpringApplication.run(ProductServiceApplication.class, args); }
+}
