7d464dc9da2e49e6baa99da5926c838810259647
Vaneth <vanemorenatropicana@gmail.com>
2025-08-23 17:51:00 -0300

    Create OrderServiceApplication.java

---
+++ order-service/src/main/java/com/marketlab/order/OrderServiceApplication.java
@@ -0,0 +1,9 @@
+package com.marketlab.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+  public static void main(String[] args) { SpringApplication.run(OrderServiceApplication.class, args); }
+}
