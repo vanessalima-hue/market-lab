7461107fc488dfe91513be1059bc605575cc044e
Vaneth <vanemorenatropicana@gmail.com>
2025-08-23 17:06:45 -0300

    Create EurekaServerApplication.java

---
+++ src/main/java/com/marketlab/eureka/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.marketlab.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaServerApplication.class, args);
+  }
+}
